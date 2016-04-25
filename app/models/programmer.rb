3d809bc7ab6193a0824ca50671c6d6b1d30e55f4
Charles Pletcher <cpletcher2@gmail.com>
2016-04-25 16:53:14 -0400

    Don't define methods that obscure a model's intent
    Fixes #6

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,11 +1,2 @@
 class Programmer < ActiveRecord::Base
-  def attributes_for_partial
-    ['home_country', 'quote', 'claim_to_fame']
-  end
-
-  def locals_hash
-    attributes_for_partial.each_with_object({}) do |att, hash|
-      hash[att] = attributes[att]
-    end
-  end
 end
