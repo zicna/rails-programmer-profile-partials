1cc7533a7ec186f366e0a0df0ef28fecbe384107
hoffm386 <erinrhof@uw.edu>
2019-04-18 19:10:52 -0700

    updating from factory girl to factory bot

--- spec/views/partials_spec.rb
+++ spec/views/partials_spec.rb
@@ -4,13 +4,13 @@ require 'spec_helper'
 describe "the navbar partial" do
   it "renders the navbar correctly" do
     render "layouts/navbar"
-   
+
     expect(response).to include('Programmers We L<span class="glyphicon glyphicon-heart"></span>ve')
   end
 end
 
 describe "the programmer partial" do
-  let(:programmer) { FactoryGirl.create(:programmer) }
+  let(:programmer) { FactoryBot.create(:programmer) }
 
   before do
     render "programmers/programmer", :programmer => programmer
@@ -40,15 +40,15 @@ end
 
 # BONUS: Create a partial that renders a single attribute of a programmer
 xdescribe "the attribute partial" do
-  let(:programmer) { FactoryGirl.create(:programmer) }
+  let(:programmer) { FactoryBot.create(:programmer) }
 
   it "renders any attribute of the programmer" do
       view.lookup_context.prefixes = %w[programmers]
-      assign(:programmer, programmer)  
+      assign(:programmer, programmer)
       render :template => "programmers/show.html.erb"
       expect(response).not_to include("programmer.send(attribute)")
       expect(response).not_to include("programmer.home_country")
-    
+
   end
 
 end
