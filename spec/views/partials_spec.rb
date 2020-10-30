b505fea91aed228d8dd0b82d85919ee1b14addbb
Liz Burton <liz.burton147@gmail.com>
2020-10-30 16:21:32 -0400

    removes bonus test

--- spec/views/partials_spec.rb
+++ spec/views/partials_spec.rb
@@ -46,27 +46,3 @@ describe "the programmers index page" do
     expect(programmers_index).to include("<%= render @programmers %>")
   end
 end
-
-# BONUS: Create a partial that renders a single attribute of a programmer
-describe "the attribute partial" do
-  let(:programmer) { Programmer.create(
-    name: 'Alan Turing',
-    home_country: 'United Kingdom',
-    birth_date: '1912-06-23',
-    death_date: '1954-06-07',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a1/Alan_Turing_Aged_16.jpg',
-    wikipedia_page: 'https://en.wikipedia.org/wiki/Alan_Turing',
-    quote: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human.',
-    claim_to_fame: 'The Turing machine, breaking the Enigma code'
-  ) }
-
-  it "renders any attribute of the programmer" do
-      view.lookup_context.prefixes = %w[programmers]
-      assign(:programmer, programmer)
-      render :template => "programmers/show.html.erb"
-      expect(response).not_to include("programmer.send(attribute)")
-      expect(response).not_to include("programmer.home_country")
-
-  end
-
-end
